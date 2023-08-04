f665debcf7bb4a3cf7ab04f356384be15a617b04
Andrew Rink <Andrewcrink@gmail.com>
2023-08-04 08:52:50 -0400

    home page info

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,6 +2,10 @@ function Home () {
     return (
         <div>
             <h1 class="title">Elden Ring Encyclopedia</h1>
+            <br/>
+            <h3 className="title">Welcome to the Elden Ring Encyclopedia</h3>
+            <p className="title">This is a simple fan-made search tool to find basic information about most elements of the game!</p>
+            
         </div>
     )
 }
